179fba7e6ab375f0f81ba78eaf877c8c19b58097
huangdaming <861634426@qq.com>
2019-04-05 17:40:11 +0800

    second commit

---
+++ src/main/java/com/hdm/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.hdm.controller;
+
+/** 
+ * @ClassName: HelloWorld
+ * @description: 
+ * @author: huangdaming
+ * @Date: 2019年4月5日
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/test/java/com/hdm/controller/TestHelloWorld.java
@@ -0,0 +1,18 @@
+package com.hdm.controller;
+
+import org.junit.Test;
+
+/** 
+ * @ClassName: TestHelloWorld
+ * @description: 
+ * @author: huangdaming
+ * @Date: 2019年4月5日
+ */
+public class TestHelloWorld {
+	
+	@Test
+	public void testMain(){
+		
+	}
+
+}
